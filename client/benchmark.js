e361c8733316c6df9b54508df03177121f84df61
mingjun97 <470890875@qq.com>
2019-06-09 16:59:28 -0700

    Functional version.

--- client/benchmark.js
+++ client/benchmark.js
@@ -9,6 +9,7 @@ var keep_update;
 $().ready(
     function(){
         var canvas = $('#stage')[0];
+        canvas.onclick = move_client_0;
         ctx = canvas.getContext("2d");
         logBoard = $('#logs')[0];
         keep_update = $('#keep-update')[0];
@@ -46,11 +47,13 @@ $().ready(
                     bot_clients[i].updatePosition();
                 }
             }
-        }, 3000)
-        bot_clients = [new Client(0)];
+        }, 100)
+        clients();
     }
 )
-
+function move_client_0(e){
+    bot_clients[0].move([e.offsetX, e.offsetY])
+}
 function point(x, y, color){
     ctx.beginPath();
     ctx.stroestyle = 'none';
@@ -64,7 +67,8 @@ function move(){
     finished_thread = 0;
     initial_time = performance.now();
     for (var i = 0 ; i < clients_count ; i++){
-        bot_clients[i].move();
+            var tmp = [Math.floor(Math.random() * 501), Math.floor(Math.random() * 501)];
+        bot_clients[i].move(tmp);
     }
 }
 function register(){
@@ -85,7 +89,7 @@ function clients(){
     $('#counts')[0].innerText = clients_count;
     bot_clients = new Array();
     for (var i = 0 ; i < clients_count ; i++){
-        bot_clients.push(new Client(i));
+        bot_clients.push(new Client(i, fashion));
     }
 }
 
@@ -101,8 +105,71 @@ class Client {
             this.login = this.login_serverless;
             this.move = this.move_serverless;
             this.get_throughput = this.get_move_throughput;
+            this.updatePosition = this.updatePosition_serverless;
+        }else{
+            this.io = io(backend);
+            this.register = this.register_server;
+            this.login = this.login_server;
+            this.move = this.move_server;
+            this.io.on('move', this.move_callback.bind(this))
+            this.get_throughput = this.get_move_throughput_server;
+            this.updatePosition = this.updatePosition_server;
+            this.io.on('whoami', this.update_callback_handler.bind(this));
+            this.throughput_measuring = false;
         }
     }
+
+    updatePosition_server(){
+        this.io.emit('whoami');
+    }
+    update_callback_handler(data){
+        if (this.throughput_measuring){
+            this.counter++;
+            this.io.emit('whoami');
+        }
+        this.position = data.position;
+    }
+
+    register_server(){
+        this.io.emit('register', {username: this.username, password: this.username});
+    }
+
+    login_server(){
+        this.io.emit('login', {username: this.username, password: this.username});
+    }
+
+    move_callback(){
+        finished_thread ++;
+        ticks += performance.now() - this.t0;
+        if (finished_thread === clients_count){
+            initial_time = performance.now() - initial_time;
+        }
+    }
+
+    move_server(target){
+        this.t0 = performance.now();
+        this.io.emit('move', {target: target});
+    }
+
+    get_move_throughput_server(silent_mode=false){
+        this.throughput_measuring = true;
+        var last_check = 0;
+        this.counter = 0;
+        var start_time = performance.now();
+        this.io.emit('whoami');
+        var handler = function(){
+            if (!silent_mode) logBoard.innerHTML += `<br/>Throughput ${this.counter - last_check} Request/Second in no overlapping fashion.`;
+            else this.expose_number = this.counter - last_check;
+            last_check = this.counter;
+            if (this.counter < 1000) {
+                setTimeout(handler,1000);
+            }else{
+                this.throughput_measuring = false;
+            }
+        }.bind(this);
+        setTimeout(handler,1000)
+    }
+
     register_serverless(){
         $.post('https://7v5r09dlhj.execute-api.us-west-1.amazonaws.com/beta/pask-main', JSON.stringify({op: 'register', account: this.username, password: this.username}));
     }
@@ -150,7 +217,7 @@ class Client {
     draw(){
         point(...this.position, this.color);
     }
-    updatePosition(){
+    updatePosition_serverless(){
         $.post('https://7v5r09dlhj.execute-api.us-west-1.amazonaws.com/beta/pask-main', JSON.stringify({op: 'whoami', token: this.token})).done(
             function(data){
                 this.position = data.position;
